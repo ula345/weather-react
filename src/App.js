21739264454b383823e264dd4fdcbf599237c3e8
ula345 <krzeminska.ula@gmail.com>
2021-10-18 20:23:55 +0200

    search added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Weather from "./weather";
 import './App.css';
 import axios from "axios"; 
+import Search from "./search" 
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Search />
             <Weather />
 
         
